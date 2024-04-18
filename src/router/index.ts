e3b7d4753e164cb07c3a5ecdec4ca292b2ecb4bd
Viacheslav Angel <do.angelwork@gmail.com>
2024-04-18 16:15:13 +0300

    fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -25,7 +25,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.NODE_ENV === 'production' ? '/vue-vite-ts/' : '/'),
   routes
 })
 
